d2a3ba4c14c2d30df2e34c35d585f04c4d488ecb
Moojun <mgim32137@gmail.com>
2022-11-16 18:22:25 +0900

    실습 11-1. 영화 포스터 보기 1

---
+++ Chapter11/실습11-1/MainActivity.java
@@ -0,0 +1,98 @@
+package com.seoultech.project11_1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("그리드뷰 영화 포스터");
+
+        final GridView gridView = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gridAdapter = new MyGridAdapter(this);
+        gridView.setAdapter(gridAdapter);
+
+
+    }
+
+    public class MyGridAdapter extends BaseAdapter {
+
+        Context context;
+        public MyGridAdapter(Context c) {
+            context = c;
+        }
+
+        Integer[] posterID = { R.drawable.mov01, R.drawable.mov02,
+                R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+                R.drawable.mov06, R.drawable.mov07, R.drawable.mov08,
+                R.drawable.mov09, R.drawable.mov10, R.drawable.mov01,
+                R.drawable.mov02, R.drawable.mov03, R.drawable.mov04,
+                R.drawable.mov05, R.drawable.mov06, R.drawable.mov07,
+                R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03,
+                R.drawable.mov04, R.drawable.mov05, R.drawable.mov06,
+                R.drawable.mov07, R.drawable.mov08, R.drawable.mov09,
+                R.drawable.mov10 };
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(200, 300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5, 5, 5, 5);
+
+            imageView.setImageResource(posterID[position]);
+
+            final int pos = position;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = (View) View.inflate(MainActivity.this,
+                            R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[pos]);
+
+                    dlg.setTitle("큰 포스터");
+                    dlg.setIcon(R.drawable.ic_launcher_foreground);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return imageView;
+        }
+
+
+
+
+    }
+}
